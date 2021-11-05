99a253eab1d0ad6d647d6a6019dee34f6b51c509
congnc <nguyenchi.dev@gmail.com>
2021-11-06 00:14:19 +0700

    add knn

--- app.py
+++ app.py
@@ -15,60 +15,72 @@ dataset = []
 uu_dataset = {}
 ii_dataset = {}
 
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    items, ratings = load_data()
-    print(ratings)
-    print(items)
-
-    return 'check loaded'
+# @app.route('/')
+# def index():
+#     cur = mysql.connection.cursor()
+#     ratings = load_data()
+    
+#     print(ratings)
+#     return 'check loaded'
 
 @app.route('/test')
 def recommend():
     id = request.args.get('user_id')
     ratings = load_data()
     print(ratings)
+    # user_ids = list(ratings.user_id.unique()) 
+    # item_ids = list(ratings.module_item_id.unique()) 
+    # numberOfUsers =  len(user_ids)
+    # numberOfItem = len(item_ids)
+
+    w, wt, uz, pz = biparteMatrix(ratings)
+    z = graphWeightMatrix(w, wt, uz, pz)
+    print(w[0][0])
+    print(w)
+    print(z)
+    
     # result = list_predicts(str(id))
     return jsonify( {
         'data': id
 } ), 201
 
-def biparteMatrix(item_frame, ratings_frame):
+def biparteMatrix(ratings_frame):
 
     """
-       convert the items data frame into userid-items biparte adjacency graph matrix
+       convert the ratings frame into userid-items biparte adjacency graph matrix
 
     """
+    ratings_frame.rating = ratings_frame.rating.astype('float64')
 
     user_ids = list(ratings_frame.user_id.unique()) 
-    movie_ids = list(item_frame.id.unique()) 
-
+    item_ids = list(ratings_frame.module_item_id.unique()) 
     numberOfUsers =  len(user_ids)
-
-    numberOfMovies = len(movie_ids)
+    numberOfItem = len(item_ids)
     
-    
-    
-    # initialize a numpy matrix of of numberOfUsers * numberOfMovies
+    # initialize a numpy matrix of of numberOfUsers * numberOfItem
+    w_biparte = np.zeros((numberOfUsers, numberOfItem))
 
-    user_movie_biparte = np.zeros((numberOfUsers, numberOfMovies))
-
-
-    for name, group in ratings_frame.groupby(["userId", "movieId"]):
-
-        #print name 
-        #print group
-        
-        # name is a tuple (userId, movieId)
-        
-        userId, movieId = name
-
+    for name, group in ratings_frame.groupby(["user_id", "module_item_id"]):
+        userId, itemId = name
         user_index = user_ids.index(userId)
-        movie_index = movie_ids.index(movieId)
-        user_movie_biparte[user_index, movie_index] = group[["rating"]].values[0,0]
+        items_index = item_ids.index(itemId)
 
-    return user_movie_biparte
+        w_biparte[user_index, items_index] = group[["rating"]].values[0,0]/5
+
+    # initialize uz pz
+    uz = np.zeros((numberOfUsers, numberOfUsers))
+    pz = np.zeros((numberOfItem, numberOfItem))
+
+    return w_biparte, w_biparte.T, uz, pz
+
+def graphWeightMatrix(w, wt, uz, pz):
+    uzw = np.concatenate((uz, wt), axis=0)
+    wpz = np.concatenate((w, pz), axis=0)
+    # print(uzw)
+    # print('\n',wpz)
+    z = np.concatenate((uzw, wpz), axis = 1)
+    # print(z)
+    return z
 
 def load_data():
 
@@ -80,11 +92,35 @@ def load_data():
                                FROM rs_comments
                                ''',connection)
 
-    items = pd.read_sql_query ('''
-                               SELECT
-                               id, category_id, title
-                               FROM rs_product
-                               ''',connection)
+    # items = pd.read_sql_query ('''
+    #                            SELECT
+    #                            id, category_id, title
+    #                            FROM rs_product
+    #                            ''',connection)
      
-    return items, ratings
+    return ratings
     
+def similarityBasedUser(w, wt):
+    L = 2
+    uz_L = np.dot(w, wt)
+    while 0 in uz_L:
+        L += 2
+        tmp = np.dot(w, wt)
+        uz_L = np.dot(tmp, uz_L)
+    print(L)
+    print(uz_L)
+    return L, uz_L
+
+# Item based
+def similarityBasedItem(w, wt):
+    L = 2
+    pz_L = np.dot(wt,w)
+    while 0 in pz_L:
+        L += 2
+        tmp = np.dot(wt,w)
+        pz_L = np.dot(tmp, pz_L)
+    print(L)
+    print(pz_L)
+    return L, pz_L
+
+
